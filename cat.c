3db6db4a6701cf4f8b8d203c45b2b168f1f344f7
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-03-24 21:51:26 -0400

    Add cat.c from Section 7.1 Low-level I/O

---
+++ cat.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/* cat:  minimal version */
+#define SIZE		512 /* arbitrary */
+
+int main()
+{
+	char buf[SIZE];
+	int n;
+
+	while ((n = read(0, buf, sizeof buf)) > 0)
+		write(1,buf,n);
+}
