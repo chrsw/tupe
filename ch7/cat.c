9eebd0406824f0071d11940388d7a065061dd261
Chris Williams <chrsw@users.noreply.github.com>
2024-06-11 20:06:47 -0400

    Add cat from Section 7.1 Low-level I/O
    Minimal version for demonstration purposes.

---
+++ ch7/cat.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/* cat:  minimal version */
+#define SIZE		512 /* arbitrary */
+
+int main(void)
+{
+	char buf[SIZE];
+	int n;
+
+	while ((n = read(0, buf, sizeof buf)) > 0)
+		write(1,buf,n);
+	exit(0);
+}
