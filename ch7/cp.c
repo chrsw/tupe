f2fdbe253e377713ef394f879bd64cd06502ffc0
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-07-28 13:26:40 -0400

    cp: Explain why text version was modified.

--- ch7/cp.c
+++ ch7/cp.c
@@ -1,4 +1,32 @@
-/*cp:  minimal version */
+/*
+ * cp:  minimal version
+ *
+ * Description
+ *      This file is the `cp` example from Section 7.1 of the text. This is
+ *      a minimal `cp` implementation that copies a source file file1 to
+ *      a destination file file2.
+ *
+ * Run:
+ *      $ ./cp <file1> <file2>
+ *
+ * Notes:
+ *      The issue with the text version of this application is the ues of the
+ *      systems, error handling and reporting feature, error(), errno, etc.
+ *      It seems modern systems have deprecated the text's usage of this
+ *      feature so slight modifications need to be made to get the text
+ *      version to build without warnings, link without errors and run without
+ *      problems.
+ *
+ * TODO:
+ *      Fix warnings:
+ *      $ gcc -o cp cp.c
+ *      /usr/bin/ld: /tmp/cc5Ns5Nd.o: in function `error':
+ *      cp.c:(.text+0x20a): warning: `sys_errlist' is deprecated; use 
+ *      `strerror' or `strerror_r' instead
+ *      /usr/bin/ld: cp.c:(.text+0x1eb): warning: `sys_nerr' is deprecated; 
+ *      use `strerror' or `strerror_r' instead
+ */
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -11,6 +39,7 @@ char *progname;
 void error(char *, char *);
 
 
+/*cp:  minimal version */
 int main(int argc, char *argv[])
 {
     int f1, f2, n;
