3c959a7f1af7e1f3f32671886af4f95e8b338d43
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-09-01 22:26:07 -0400

    wc: fix spelling

--- ch4/wc.c
+++ ch4/wc.c
@@ -13,7 +13,7 @@
 /* data types */
 struct tnode {                  /* the tree node */
     char *word;                 /* points to the text */
-    int count;                  /* number of occurrances */
+    int count;                  /* number of occurrences */
     int rank;
     struct tnode *left;         /* left child */
     struct tnode *right;        /* right child */
