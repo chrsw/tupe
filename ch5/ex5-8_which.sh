cc7bb8678b39769762e31269b87bfcd82b1f7bc4
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-09-08 16:41:16 -0400

    Solution 5-8: 'which' exec check, print errors.

---
+++ ch5/ex5-8_which.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# which cmd:  which cmd in PATH is executed
+# Exercise 5-8: check file for exec permissions
+#               print an error if no file found
+
+opath=$PATH
+PATH=/bin:/usr/bin
+
+case $# in
+	0)	echo 'Usage: which command' 1>&2; exit 2
+esac
+for i in `echo $opath | sed 's/^:/.:/
+			     s/::/:.:/g
+			     s/:$/:./
+			     s/:/ /g'`
+do
+	if test -x $i/$1	# this is /bin/test
+	then			# or /usr/bin/test only
+		echo $i/$1
+		exit 0		# found it
+	fi
+done
+
+echo "which: $1 not found" 1>&2; exit 1
+
