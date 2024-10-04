a840fe01254194ec4005ed6054ef707da62f747c
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-10-03 21:07:16 -0400

    Solution 6-1 Escape non-printable characters

--- ch6/ex6-1_vis.c
+++ ch6/ex6-1_vis.c
@@ -12,12 +12,23 @@
  * 
  */
 
+/* Escape sequences that will be used in this exercise are from this ASCII 
+ * table snippet:
+ *     007   7     07    BEL '\a' (bell)
+ *     010   8     08    BS  '\b' (backspace)
+ *     011   9     09    HT  '\t' (horizontal tab)
+ *     012   10    0A    LF  '\n' (new line)
+ *     013   11    0B    VT  '\v' (vertical tab)
+ *     014   12    0C    FF  '\f' (form feed)
+ *     015   13    0D    CR  '\r' (carriage ret)
+ */
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-#define MAX_LINE_LEN 4096
+#define MAX_LINE_LEN 4095
 
 char blankbuf[MAX_LINE_LEN];
 
@@ -27,6 +38,32 @@ int main(void)
     int i = 0;
 	
     while ((c = getchar()) != EOF) {
+        switch (c) {
+            case 7:
+                puts("\a");
+                break;
+            case 8:
+                puts("\b");
+                break;
+            case 9:
+                puts("\t");
+                break;
+            case 10:
+                puts("\n");
+                break;
+            case 11:
+                puts("\v");
+                break;
+            case 12:
+                puts("\f");
+                break;
+            case 13:
+                puts("\r");
+                break;
+            default:
+                break;
+        }
+
         if (c == ' ' || c == '\t')
             blankbuf[i++] = (char)c;
         else if (c == '\n'){
