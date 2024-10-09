83cabe949db9d18ef6d5c8f8cc1c79411a4a5d3b
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-10-08 20:43:05 -0400

    Add vis version 2 from Section 6.2

---
+++ ch6/sec6_2_vis.c
@@ -0,0 +1,24 @@
+/* ex6_2_vis:  make funny characters visible (version 2) 
+               add an option to strip non-printing charcters
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    int c, strip = 0;
+    
+    if (argc > 1 && strcmp(argv[1], "-s") == 0)
+        strip = 1;
+    while ((c = getchar()) != EOF)
+        if (isascii(c) && 
+            (isprint(c) || c == '\n' || c == '\t' || c == ' '))
+            putchar(c);
+        else if (!strip)
+            printf("\\%03o", c);
+    return 0;
+}
