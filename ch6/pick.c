63048ba5429af627e69ac58e50f28cde56adc118
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-11-22 23:44:29 -0500

    pick: cleanups
    Fix compiler warnings.
    Make formatting more like text.
    Make style more up-to-date.
    Add build command.

--- ch6/pick.c
+++ ch6/pick.c
@@ -1,39 +1,41 @@
 /* pick: offer choice on each argument */
 
+/* gcc -o pick pick.c efopen.c ttyin.c */
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 static void pick(char *);
 char ttyin();
 FILE * efopen(char, int);
 
-#define BUFSIZE 100
 
 char *progname;         /* program name for error messages */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int i;
+    char buf[BUFSIZ];
 
-        int i;
-        char buf[BUFSIZE];
+    progname = argv[0];
+    if (argc == 2 && strcmp(argv[1],"-") == 0) /* pick - */
+        while (fgets(buf, sizeof(buf), stdin) != NULL) {
+            buf[strlen(buf)-1] = '\0'; /* drop newline */
+            pick(buf);
+        }
+    else
+        for (i = 1; i < argc; i++)
+            pick(argv[i]);
 
-        progname = argv[0];
-        if (argc == 2 && strcmp(argv[1],"-") == 0) /* pick - */
-                while (fgets(buf, sizeof(buf), stdin) != NULL) {
-                        buf[strlen(buf)-1] = '\0'; /* drop newline*/
-                        pick(buf);
-                }
-        else
-                for (i = 1; i < argc; i++)
-                        pick(argv[i]);
-
-        return 0;
+        exit(0);
 }
 
-
-void pick(char *s){
-
-        fprintf(stderr, "", s);
-        if(ttyin() == 'y')
-                printf("%s\n", s);
+/* pick:  offer choice of s */
+void pick(char *s)
+{
+    fprintf(stderr, "%s?", s);
+    if(ttyin() == 'y')
+        printf("%s\n", s);
 }
 
